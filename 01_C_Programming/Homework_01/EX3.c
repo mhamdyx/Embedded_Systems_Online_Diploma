ffb88c332588c86a3459b1a6423b21a2face9dbb
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-12-18 02:31:46 +0200

    add Homework1/Exercise3

---
+++ 01_C_Programming/Homework_01/EX3.c
@@ -0,0 +1,21 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Dec 18, 2020
+ *      Author: MahmoudH
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n1, n2;
+
+	printf("Enter two integers: ");
+    
+	scanf("%d %d", &n1, &n2);
+
+	printf("Sum: %d\n", n1 + n2);
+
+	return 0;
+}

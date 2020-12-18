28db455fc2e419b3d619ce1a195dc7a871fd8fe8
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-12-18 02:24:40 +0200

    add Homework1/Exercise2

---
+++ 01_C_Programming/Homework_01/EX2.c
@@ -0,0 +1,21 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Dec 18, 2020
+ *      Author: MahmoudH
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("Enter an integer: ");
+	
+    scanf("%d", &n);
+
+	printf("You entered %d\n", n);
+
+	return 0;
+}

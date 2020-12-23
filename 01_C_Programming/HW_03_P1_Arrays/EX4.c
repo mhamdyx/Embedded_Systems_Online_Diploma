2973505d30a55b87f6ae5a0922dc5305b20d2f50
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-12-23 04:33:21 +0200

    add HW3_Part1/Exercise4

---
+++ 01_C_Programming/HW_03_P1_Arrays/EX4.c
@@ -0,0 +1,51 @@
+/*
+ * EX4.c
+ *
+ *  Created on: Dec 23, 2020
+ *      Author: MahmoudH
+ */
+
+#include <stdio.h>
+
+#define MAX_LEN 100
+
+int main(void)
+{
+	int arr[MAX_LEN], arr_len, extra_element, position, i;
+
+	printf("Enter no of elements: ");
+	scanf("%d", &arr_len);
+
+	printf("\n");
+
+	for(i = 0; i < arr_len; i++)
+	{
+		arr[i] = i + 1;
+		printf("%d ", arr[i]);
+	}
+
+	printf("\r\nEnter the element to be inserted: ");
+	scanf("%d", &extra_element);
+
+	printf("\nEnter the location: ");
+	scanf("%d", &position);
+
+	printf("\n");
+
+	for(i = arr_len; i >= position; i--)
+	{
+		arr[i] = arr[i - 1];
+	}
+
+	arr[position - 1] = extra_element;
+	arr_len++;
+
+	for(i = 0; i < arr_len; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	printf("\n");
+
+	return 0;
+}

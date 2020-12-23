3544db5a5e15430d4b89fb5a8e24e9600ac76bef
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2020-12-23 06:55:56 +0200

    add HW3_Part2/Exercise3

---
+++ 01_C_Programming/HW_03_P2_Strings/EX3.c
@@ -0,0 +1,34 @@
+/*
+ * EX3.c
+ *
+ *  Created on: Dec 23, 2020
+ *      Author: MahmoudH
+ */
+
+#include <stdio.h>
+
+#define MAX_LEN 100
+
+int main(void)
+{
+	char input_string[MAX_LEN];
+	int len = 0, left, right;
+
+	printf("Enter the string: ");
+	gets(input_string);
+
+	while(input_string[len++]);
+	len--;
+
+	for(left = 0, right = len - 1; left < right; left++, right--)
+	{
+
+		input_string[left] ^= input_string[right];
+		input_string[right] ^= input_string[left];
+		input_string[left] ^= input_string[right];
+	}
+
+	printf("Reverse string is: %s\n", input_string);
+
+	return 0;
+}

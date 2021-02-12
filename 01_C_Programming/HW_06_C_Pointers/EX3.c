f921754880e813e99245524e2f5b7640008f96b6
SuperMoudy <mahmoudhamdy.eeng@gmail.com>
2021-02-12 16:50:46 +0200

    add HW6/Exercise3

---
+++ 01_C_Programming/HW_06_C_Pointers/EX3.c
@@ -0,0 +1,32 @@
+/*
+ * EX3.c
+ *
+ *  Created on: Feb 12, 2021
+ *      Author: MahmoudH
+ */
+
+#include <stdio.h>
+#define LEN 30
+
+int main(void)
+{
+	char ch_str[LEN + 1];
+	char *ptr = ch_str;
+
+	printf("Input a string : ");
+	scanf("%s", ch_str);
+
+	/* Seeking the end of the string */
+    while(*ptr++);
+    
+    /* Adjusting to the end of the string */
+    ptr--;
+
+	printf("Reverse of the string is : ");
+	while(ptr != ch_str)
+	{
+		printf("%c", *--ptr);
+	}
+
+	return 0;
+}
